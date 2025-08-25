a9e205a5f1dbf51f8f66aecc8c79957ac53be1ea
komalverma893 <bk4937364@gmail.com>
2025-08-25 12:23:20 +0530

    h

--- project.jsx
+++ project.jsx
@@ -5,6 +5,7 @@ function Project(){
     <>
     <title>tripma</title>
     {/*nav*/}
+    <div>
     <div>Join Tripma today and save up to 20% on your flight using code TRAVEL at checkout. Promotion valid for new users only.    </div>
 <div>
     <div>TRIPMA</div>
@@ -15,7 +16,27 @@ function Project(){
       <li><a href="#">Sign in</a></li>
     </ul>
     <button class="btn-signup">Sign up</button></div>
+       
+
+<div>
+    <h1>It’s more than<br/>just a trip</h1>
+    </div>
+
+
+    <div >
+      <input type="text" placeholder="From where?"></input>
+      <input type="text" placeholder="Where to?"></input>
+      <input type="text" placeholder="Depart - Return"></input>
+      <select>
+        <option>1 adult</option>
+        <option>2 adults</option>
+        <option>3 adults</option>
+      </select>
+      <button>Search</button>
+    </div>
+    </div>
 </div>
+ 
     </>
    ) 
 }
