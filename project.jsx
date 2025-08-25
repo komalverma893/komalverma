3badb1d7437b0d9e83e4eced76cee50f9dd01da3
komalverma893 <bk4937364@gmail.com>
2025-08-25 12:21:13 +0530

    h

---
+++ project.jsx
@@ -0,0 +1,22 @@
+//hello 
+import React from "react";
+function Project(){
+   return(
+    <>
+    <title>tripma</title>
+    {/*nav*/}
+    <div>Join Tripma today and save up to 20% on your flight using code TRAVEL at checkout. Promotion valid for new users only.    </div>
+<div>
+    <div>TRIPMA</div>
+    <div><ul>
+      <li><a href="#">Flights</a></li>
+      <li><a href="#">Hotels</a></li>
+      <li><a href="#">Packages</a></li>
+      <li><a href="#">Sign in</a></li>
+    </ul>
+    <button class="btn-signup">Sign up</button></div>
+</div>
+    </>
+   ) 
+}
+export default Project;
